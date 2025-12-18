2610e7deca9c06c6c699df229ced837541441b27
Bruno Gonçalves <bruno.goncalves1999@hotmail.com>
2025-12-18 15:35:29 -0300

    Add controllers e Views

---
+++ Organizzae.Web/Controllers/ObjetivosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Organizzae.Web.Controllers
+{
+    public class ObjetivosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
